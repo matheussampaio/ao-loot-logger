613ed6d8e52314cea464fdcb3d3226dcaab2917f
Matheus Sampaio <matheus@sampaio.us>
2021-06-25 15:49:29 -0500

    feat: parse tag_name to better detect when a new version is available

--- src/check-new-version.js
+++ src/check-new-version.js
@@ -24,9 +24,38 @@ async function checkNewVersion() {
   const { data } = response
 
   if (data.tag_name !== package.version) {
-    console.info(
-      'New version available. Download it from https://github.com/matheussampaio/ao-loot-logger/releases/latest'
-    )
+    const latestVersion = parseVersion(data.tag_name)
+    const currentVersion = parseVersion(package.version)
+
+    const isNewVersionAvailable =
+      currentVersion.major < latestVersion.major ||
+      currentVersion.minor < latestVersion.minor ||
+      currentVersion.patch < latestVersion.patch
+
+    if (isNewVersionAvailable) {
+      console.info(
+        `\x1b[32mNew AO Loot Logger ${data.tag_name} available: https://github.com/matheussampaio/ao-loot-logger/releases/latest\x1b[0m\n`
+      )
+    }
+  }
+}
+
+function parseVersion(version) {
+  const re = /^v?(?<major>\d+).(?<minor>\d+).(?<patch>\d+)/
+  const match = version.match(re)
+
+  if (match == null) {
+    return {
+      major: 0,
+      minor: 0,
+      patch: 0
+    }
+  }
+
+  return {
+    major: parseInt(match.groups.major, 10),
+    minor: parseInt(match.groups.minor, 10),
+    patch: parseInt(match.groups.patch, 10)
   }
 }
 
