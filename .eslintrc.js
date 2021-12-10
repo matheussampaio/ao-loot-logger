d787d0a9031901e5764bcad366ba48cda5984cbf
Matheus Sampaio <matheus@sampaio.us>
2021-12-09 21:56:41 -0800

    chore: add eslint configurations

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 13
+  },
+  rules: {
+    'no-unused-vars': 0
+  }
+}
